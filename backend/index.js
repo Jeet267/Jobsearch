c95716190200c85620e42bd03eee4fda6f767faf
Jeet267 <ajtkr200@gmail.com>
2024-11-11 00:33:23 +0530

    a

--- backend/index.js
+++ backend/index.js
@@ -7,10 +7,12 @@ import userRoute from "./routes/user.routes.js";
 import companyRoute from "./routes/company.routes.js";
 import jobRoute from "./routes/job.routes.js";
 import applicationRoute from "./routes/application.routes.js"
-
+import path from "path"
 dotenv.config({});
 
 const app = express();
+const __dirname = path.resolve()
+
 // middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -37,6 +39,11 @@ app.use("/api/v1/user", userRoute);
 app.use("/api/v1/company", companyRoute);
 app.use("/api/v1/job", jobRoute);
 app.use("/api/v1/application",applicationRoute);
+app.use(express.static(path.join(__dirname,"/frontend/dist")))
+
+app.get("*",(_,res)=>{
+    res.sendFile(path.join(__dirname,"/frontend/dist/index.html"))
+})
 
 app.listen(port, () => {
     dbConnect();
