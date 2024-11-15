6a0c7f77ec8626aa8edeb18a99b1c6894ebc2447
Jeet267 <ajtkr200@gmail.com>
2024-11-15 15:36:04 +0530

    solved cors

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
 const corsOptions = {
-    origin:'http://localhost:5173',
+    origin:'https://jobsearch-khaki.vercel.app',
     credentials:true
 }
 
